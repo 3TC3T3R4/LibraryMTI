064d3e5e38da04bb15a89f0f449b57ad2dfbbbbc
Estevan <teban1928@gmail.com>
2025-02-11 12:33:15 -0500

    Inyection Dependeci patter

--- Library.Host.Api/Program.cs
+++ Library.Host.Api/Program.cs
@@ -1,3 +1,6 @@
+using Library.Infraestructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,15 +9,28 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//Esto es Inyeccion de dependencias
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefatultConnection"));
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+using (var scope = app.Services.CreateScope()) { 
+
+   // var services = scope.ServiceProvider;
+    var context =  scope.ServiceProvider.GetRequiredService<LibraryDbContext>();
+    context.Database.Migrate();
+
 }
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
 app.UseHttpsRedirection();
 
